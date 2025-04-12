39118f5c798980bf536d57772ff31c066e493a2b
柠檬冲水 <ningmengchongshui@gmail.com>
2025-04-13 05:34:01 +0800

    Update miao.ts

--- src/miao.ts
+++ src/miao.ts
@@ -1,11 +1,22 @@
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
+import { join } from 'path'
 /**
  * ************
  * 依赖于喵喵插件
  * ************
  */
-const components = require.resolve('#miao')
-const models = require.resolve('#miao.models')
+const components = join(
+  process.cwd(),
+  'plugins',
+  'miao-plugin',
+  'components',
+  'index.js'
+)
+const models = join(
+  process.cwd(),
+  'plugins',
+  'miao-plugin',
+  'models',
+  'index.js'
+)
 export const { Data } = await import(`file://${components}`)
 export const { Character, Weapon, Player } = await import(`file://${models}`)
